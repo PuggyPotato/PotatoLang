94bd8b885fdb3dbf2fc0ac809df1050cc84aeb85
Puggy Potato <qingyaurou@gmail.com>
2026-08-20 02:28:04 +0800

    Initial Commit:Defined basic structure of token

---
+++ token/token.go
@@ -0,0 +1,69 @@
+package token
+
+type TokenType string
+
+type Token struct {
+	Type TokenType
+	Literal string
+}
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF = "EOF"
+
+	// Identifiers + Literals
+	IDENT = "IDENT"
+	NUMBER = "NUMBER" // 1, 2, 3, 4, 1.1, 1.2 ...
+	STRING = "STRING"
+
+	// Operators
+	ASSIGN = "="
+	PLUS = "+"
+	MINUS = "-"
+	BANG = "!"
+	ASTERISK = "*"
+	SLASH = "/"
+	LT = "<"
+	GT = ">"
+	EQ = "=="
+	NOT_EQ = "!="
+
+	// Delimiters
+	COMMA = ","
+	SEMICOLON = ";"
+	COLON = ":"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	// Keywords
+	FUNCTION = "FUNCTION"
+	LET = "LET"
+	TRUE = "TRUE"
+	FALSE = "FALSE"
+	IF = "IF"
+	ELSE = "ELSE"
+	RETURN = "RETURN"
+	MUT = "MUT"
+
+)
+
+var keywords = map[string] TokenType {
+	"func": FUNCTION,
+	"let": LET,
+	"true": TRUE,
+	"false": FALSE,
+	"if": IF,
+	"else": ELSE,
+	"return": RETURN,
+	"mut": MUT,
+}
+
+func LookupIdent(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return IDENT
+}
