9149ad7e615cf16a00627365f63e10ebb450a01e
Puggy Potato <qingyaurou@gmail.com>
2026-08-20 17:46:24 +0800

    feat: add lexer support for numbers, identifiers, and whitespace
    Implement whitespace skipping for proper token separation.
    Add scanning for identifying literals.
    Add number parsing, treating all numeric values as float64.
    Add TODO for the return type arrow operator to be implemented with the minus operator.

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -6,18 +6,72 @@ import (
 )
 
 func TestNextToken(t *testing.T) {
-	input := "=+(){};"
+	// Todo: Add arrow return types to the function
+	// i.e. func(x, y) -> number { return x + y }
+	input := `
+				let x = 5;
+				let y = 10.5;
+
+				let add = func(x, y) {
+					return x + y;
+				}
+				
+				let z = add(x,y);
+
+				let mut a = 5;
+				a = 100.50;
+`
 
 	tests := []struct {
 		expectedType token.TokenType
 		expectedLiteral string
 	} {
+		{token.LET, "let"},
+		{token.IDENT, "x"},
 		{token.ASSIGN, "="},
-		{token.PLUS, "+"},
+		{token.NUMBER, "5"},
+		{token.SEMICOLON, ";"},
+		{token.LET, "let"},
+		{token.IDENT, "y"},
+		{token.ASSIGN, "="},
+		{token.NUMBER, "10.5"},
+		{token.SEMICOLON, ";"},
+		{token.LET, "let"},
+		{token.IDENT, "add"},
+		{token.ASSIGN, "="},
+		{token.FUNCTION, "func"},
 		{token.LPAREN, "("},
+		{token.IDENT, "x"},
+		{token.COMMA, ","},
+		{token.IDENT, "y"},
 		{token.RPAREN, ")"},
 		{token.LBRACE, "{"},
+		{token.RETURN, "return"},
+		{token.IDENT, "x"},
+		{token.PLUS, "+"},
+		{token.IDENT, "y"},
+		{token.SEMICOLON, ";"},
 		{token.RBRACE, "}"},
+		{token.LET, "let"},
+		{token.IDENT, "z"},
+		{token.ASSIGN, "="},
+		{token.IDENT, "add"},
+		{token.LPAREN, "("},
+		{token.IDENT, "x"},
+		{token.COMMA, ","},
+		{token.IDENT, "y"},
+		{token.RPAREN, ")"},
+		{token.SEMICOLON, ";"},
+		{token.LET, "let"},
+		{token.MUT, "mut"},
+		{token.IDENT, "a"},
+		{token.ASSIGN, "="},
+		{token.NUMBER, "5"},
+		{token.SEMICOLON, ";"},
+		{token.IDENT, "a"},
+		{token.ASSIGN, "="},
+		{token.NUMBER, "100.50"},
+		{token.SEMICOLON, ";"},
 	}
 
 	l := New(input)
