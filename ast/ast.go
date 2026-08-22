c3aa9dba346f2df6352fc53522bf017ff8e4c1e3
Puggy Potato <qingyaurou@gmail.com>
2026-08-22 17:30:40 +0800

    test: implement AST string conversion test
    - Hand-craft an AST for a let statement to verify the String() methods
    
    - Fix struct literal syntax to explicitly name fields for token.Token and ast.Identifier
    
    - Ensure multi-variable let statements reconstruct into valid source code strings

--- ast/ast.go
+++ ast/ast.go
@@ -1,9 +1,14 @@
 package ast
 
-import "potatolang/token"
+import (
+	"bytes"
+	"potatolang/token"
+	"strings"
+)
 
 type Node interface {
 	TokenLiteral() string
+	String() string
 }
 
 type Statement interface {
@@ -28,6 +33,16 @@ func (p *Program) TokenLiteral() string {
 	}
 }
 
+func (p *Program) String() string {
+	var out bytes.Buffer
+
+	for _, s := range p.Statements {
+		out.WriteString(s.String())
+	}
+
+	return out.String()
+}
+
 type LetStatement struct {
 	Token token.Token // the token.LET token 
 	Names []*Identifier
@@ -36,6 +51,32 @@ type LetStatement struct {
 
 func (ls *LetStatement) statementNode() {}
 func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
+func (ls *LetStatement) String() string {
+	var out bytes.Buffer
+
+	out.WriteString(ls.TokenLiteral() + " ")
+
+	var names []string
+	for _, name := range ls.Names {
+		names = append(names, name.Value)
+	}
+	out.WriteString(strings.Join(names, ", "))
+	out.WriteString(" = ")
+
+	if len(ls.Values) > 0 {
+		var values []string
+		for _, val := range ls.Values {
+			if val != nil {
+				values = append(values, val.String())
+			}
+		}
+		out.WriteString(strings.Join(values, ", "))
+	}
+
+	out.WriteString(";")
+
+	return out.String()
+}
 
 type Identifier struct {
 	Token token.Token // the token.IDENT token 
@@ -45,12 +86,45 @@ type Identifier struct {
 
 func (i *Identifier) expressionNode() {}
 func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
+func (i *Identifier) String() string { return i.Value }
 
 
 type ReturnStatement struct {
 	Token token.Token // the 'return' token 
-	ReturnValue []Expression
+	ReturnValues []Expression
 }
 
 func (rs *ReturnStatement) statementNode () {}
 func (rs *ReturnStatement) TokenLiteral() string { return rs.Token.Literal }
+func (rs *ReturnStatement) String() string {
+	var out bytes.Buffer
+
+	out.WriteString(rs.TokenLiteral() + " ")
+
+
+	if len(rs.ReturnValues) > 0 {
+		var values []string
+		for _, value := range rs.ReturnValues {
+			values = append(values, value.String())
+		}
+		out.WriteString(strings.Join(values, ", "))
+	}
+
+	out.WriteString(";")
+	
+	return out.String()
+}
+
+type ExpressionStatement struct {
+	Token token.Token // the first token of the expression
+	Expression Expression
+}
+
+func (es *ExpressionStatement) expressionNode() {}
+func (es *ExpressionStatement) TokenLiteral() string { return es.Token.Literal }
+func (es *ExpressionStatement) String() string {
+	if es.Expression != nil {
+		return es.Expression.String()
+	}
+	return ""
+}
---
+++ ast/ast_test.go
@@ -0,0 +1,33 @@
+package ast
+
+import (
+	"potatolang/token"
+	"testing"
+)
+
+func TestString(t *testing.T) {
+	program := &Program{
+		Statements: []Statement{
+			&LetStatement{
+				Token: token.Token{Type: token.LET, Literal: "let"},
+				Names: []*Identifier {
+					&Identifier{
+						Token: token.Token{Type: token.IDENT, Literal: "myVar"},
+						Value: "myVar",
+						IsMut: false,
+					},
+				},
+				Values: []Expression{
+					&Identifier {
+						Token: token.Token{Type: token.IDENT, Literal: "anotherVar"},
+						Value: "anotherVar",
+					},
+				},
+			},
+		},
+	}
+
+	if program.String() != "let myVar = anotherVar;" {
+		t.Errorf("program.String() wrong. got=%q", program.String())
+	}
+}
