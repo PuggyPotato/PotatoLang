32efbd0b7702015ae37c8dd072283a62f6ce7179
Puggy Potato <qingyaurou@gmail.com>
2026-08-22 16:53:45 +0800

    feat: implement basic return statement parsing
    - Add ReturnStatement AST node with []Expression to support multiple returns
    
    - Update parseStatement router to handle token.RETURN
    
    - Add parseReturnStatement function (skipping expressions for now)
    
    - Add TestReturnStatements to verify the AST structure and tokens

--- parser/parser.go
+++ parser/parser.go
@@ -53,6 +53,8 @@ func (p *Parser) parseStatement() ast.Statement {
 	switch p.curToken.Type {
 		case token.LET:
 			return p.parseLetStatement()
+		case token.RETURN:
+			return p.parseReturnStatement()
 		default:
 			return nil
 	}
@@ -123,4 +125,17 @@ func (p *Parser) Errors() []string {
 func (p *Parser) peekError(t token.TokenType) {
 	msg := fmt.Sprintf("expected next token to be %s, got %s instead", t, p.peekToken.Type)
 	p.errors = append(p.errors, msg)
+}
+
+func (p *Parser) parseReturnStatement() *ast.ReturnStatement {
+	stmt := &ast.ReturnStatement{Token: p.curToken}
+
+	p.NextToken() // Move past the 'return' token  
+
+	// TODO : skipping the expression until encountering a semicolon
+	for !p.curTokenIs(token.SEMICOLON) {
+		p.NextToken()
+	}
+
+	return stmt
 }
