09100795155d7c4b3aeb0512d2e812f9209f58d6
Puggy Potato <qingyaurou@gmail.com>
2026-08-23 17:15:09 +0800

    feat: support evaluation of infix operators
    - Update evaluator to handle ast.InfixExpression nodes
    
    - Implement integer arithmetic (+, -, *, /) and comparisons (<, >)
    
    - Add boolean equality checks (==, !=)
    
    - Add TestEvalInfixExpression to verify math and comparison logic

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -29,6 +29,10 @@ func Eval(node ast.Node) object.Object {
 		case *ast.PrefixExpression:
 			right := Eval(node.Right)
 			return evalPrefixExpression(node.Operator, right)
+		case *ast.InfixExpression:
+			left := Eval(node.Left)
+			right := Eval(node.Right)
+			return evalInfixExpression(node.Operator, left, right)
 			
 	}
 	return nil
@@ -82,4 +86,43 @@ func evalMinusOperatorExpression(right object.Object) object.Object {
 
 	value := right.(*object.Number).Value
 	return &object.Number{Value: -value}
+}
+
+func evalInfixExpression(operator string, left object.Object, right object.Object) object.Object {
+	switch {
+		case left.Type() == object.NUMBER_OBJ && right.Type() == object.NUMBER_OBJ:
+			return evalNumberInfixExpression(operator, left, right)
+		case operator == "==":
+			return nativeBoolToBooleanObject(left == right)
+		case operator == "!=":
+			return nativeBoolToBooleanObject(left != right)
+		default:
+			return NIL
+	}
+}
+
+func evalNumberInfixExpression(operator string, left object.Object, right object.Object) object.Object {
+	leftVal := left.(*object.Number).Value
+	rightVal := right.(*object.Number).Value
+
+	switch operator {
+		case "+":
+			return &object.Number{Value: leftVal + rightVal }
+		case "-":
+			return &object.Number{Value: leftVal - rightVal }
+		case "*":
+			return &object.Number{Value: leftVal * rightVal }
+		case "/":
+			return &object.Number{Value: leftVal / rightVal }
+		case "<":
+			return nativeBoolToBooleanObject(leftVal < rightVal)
+		case ">":
+			return nativeBoolToBooleanObject(leftVal > rightVal)
+		case "==":
+			return nativeBoolToBooleanObject(leftVal == rightVal)
+		case "!=":
+			return nativeBoolToBooleanObject(leftVal != rightVal)
+		default:
+			return NIL
+	}
 }
--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -17,6 +17,17 @@ func TestEvalNumberExpression(t *testing.T) {
 		{"69.54", 69.54},
 		{"-5.12", -5.12},
 		{"-10", -10},
+		{"5 + 5 + 5 + 5 - 10", 10},
+		{"2 * 2 * 2 * 2 * 2", 32},
+		{"-50.67 + 100.67 + -50", 0},
+		{"5 * 2 + 10.23", 20.23},
+		{"5 + 2 * 10", 25},
+		{"20 + 2 * -10", 0},
+		{"50 / 2 * 2 + 10.92", 60.92},
+		{"2 * (5 + 10)", 30},
+		{"3 * 3 * 3 + 10", 37},
+		{"3 * (3 * 3) + 15.67", 42.67},
+		{"(5 + 10 * 2 + 15 / 3) * 2 + -10", 50},
 	}
 
 	for _, tt := range tests {
@@ -55,6 +66,22 @@ func TestEvalBooleanExpression(t *testing.T) {
 	} {
 		{"true", true},
 		{"false", false},
+		{"1 < 2", true},
+		{"1 > 2", false},
+		{"1 < 1", false},
+		{"1 > 1", false},
+		{"1 == 1", true},
+		{"1 != 1", false},
+		{"1 == 2", false},
+		{"1 != 2", true},
+		{"false == false", true},
+		{"true == false", false},
+		{"true != false", true},
+		{"false != true", true},
+		{"(1 < 2) == true", true},
+		{"(1 < 2) == false", false},
+		{"(1 > 2) == true", false},
+		{"(1 > 2) == false", true},
 	}
 
 	for _, tt := range tests {
