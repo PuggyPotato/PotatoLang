27b7cd0ee484c07b7dd26f4babcb473a4439a755
Puggy Potato <qingyaurou@gmail.com>
2026-08-23 16:42:23 +0800

    feat: integrate evaluator into interactive REPL
    - Update repl.go to pass parsed AST to evaluator.Eval
    
    - Output object.Inspect() values instead of stringified AST
    
    - Enable real-time execution and visualization of evaluated code

--- repl/repl.go
+++ repl/repl.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+	"potatolang/evaluator"
 	"potatolang/lexer"
 	"potatolang/parser"
 )
@@ -29,9 +30,12 @@ func Start(in io.Reader, out io.Writer) {
 			printParserErrors(out, p.Errors())
 			continue
 		}
-
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
 		
 	}
 }
